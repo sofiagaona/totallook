4c55496c5eecb91b3e00d42b2ab95fcf710843e5
Ana Moreno Nuñez <anasofiamorenonunez@outlook.com>
2022-01-15 13:12:06 -0600

    Start Project

---
+++ financialeducation/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'financialeducation-f5611',
+    appId: '1:116708000502:web:b81c9d5d953855e16062d7',
+    storageBucket: 'financialeducation-f5611.appspot.com',
+    apiKey: 'AIzaSyD1I9OX8zUpMj3WbFOhQCvqTNCu_4NrEas',
+    authDomain: 'financialeducation-f5611.firebaseapp.com',
+    messagingSenderId: '116708000502',
+  },
+  production: true
+};
